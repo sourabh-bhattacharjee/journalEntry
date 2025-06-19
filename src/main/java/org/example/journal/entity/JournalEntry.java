c87d0e29764f1019ac6a6f7ba0eccd1fc90106ae
sourabhrockz <sourabh20051998@gmail.com>
2025-06-19 18:58:18 +0200

    integrating lombok into project

--- src/main/java/org/example/journal/entity/JournalEntry.java
+++ src/main/java/org/example/journal/entity/JournalEntry.java
@@ -1,11 +1,15 @@
 package org.example.journal.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
+@Setter
+@Getter
 @Document(collection = "journal_entries")
 public class JournalEntry {
 
@@ -14,38 +18,5 @@ public class JournalEntry {
     private String title;
     private String content;
     private Date date;
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
 
 }
